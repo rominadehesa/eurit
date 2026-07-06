77c4cc9bc1e96f8c5b6a71e3fba018b59c55ba47
Romina Dehesa <rominadehesa@gmail.com>
2026-07-06 16:02:06 -0300

    fix

---
+++ nav-mobile.js
@@ -0,0 +1,28 @@
+const btnBurger = document.getElementById('btn-burger');
+const nav = document.getElementById('nav-mobile');
+const header = document.querySelector('nav');
+
+btnBurger.addEventListener('click', function (e) {
+    e.stopPropagation(); // Evita que el click llegue al document
+
+    nav.classList.toggle('active');
+    header.classList.toggle('active');
+
+    if (nav.classList.contains('active')) {
+        nav.style.padding = "2vh 4vw";
+    } else {
+        nav.style.padding = "";
+    }
+});
+
+// Evita que se cierre al hacer click dentro del menú
+nav.addEventListener('click', function (e) {
+    e.stopPropagation();
+});
+
+// Cierra el menú al hacer click en cualquier otra parte
+document.addEventListener('click', function () {
+    nav.classList.remove('active');
+    header.classList.remove('active');
+    nav.style.padding = "";
+});
